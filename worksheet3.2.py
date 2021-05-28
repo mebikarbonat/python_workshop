b12cf37d7b0f89d6a4aa50fef6e463a053cbcb4a
Muhammad Azizi Mohd Ariffin <azizi.mohdariffin@gmail.com>
2021-05-29 01:42:59 +0800

    3.2

---
+++ worksheet3.2.py
@@ -0,0 +1,13 @@
+#Muhammad Azizi Mohd Ariffin
+#mazizi@fskm.uitm.edu.my
+
+#This will print out hello using While loop
+count = 0
+while count < 5:
+    print('hello')
+    count += 1
+
+#This will print all the value in the dictionary
+rasul = ['adam', 'idris', 'nuh', 'hud', 'salih']
+for name in rasul :
+    print(name)
