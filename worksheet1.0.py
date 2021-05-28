62ac0b2a361bfefc2e7b858e6eccec2c71e997b3
Muhammad Azizi Mohd Ariffin <azizi.mohdariffin@gmail.com>
2021-05-28 22:13:06 +0800

    1.1

--- worksheet1.0.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu May 27 21:30:11 2021
-
-@author: azizi
-"""
-
---
+++ worksheet1.1.py
@@ -0,0 +1,14 @@
+import platform
+
+def printOutPlatform():
+    print("Hey I'm in the function code block")
+    print("Your Computer Platform Type is :" + str(platform.platform()))
+a = 1  
+while True :
+    print("Hey I'm in the while statement code block")
+    
+    if a == 1 :
+        print("Hey I'm in the if statement code block")
+        printOutPlatform()
+    
+    break
