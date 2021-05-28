7990fa82148ee047544f634dc128bacf56767323
Muhammad Azizi Mohd Ariffin <azizi.mohdariffin@gmail.com>
2021-05-29 01:10:38 +0800

    3,1

--- worksheet3.1.py
+++ worksheet3.1.py
@@ -5,8 +5,13 @@ rasul = ['adam', 'idris', 'nuh', 'hud', 'salih']
 print(rasul)
 #Printing value at index 3
 print(rasul[3])
+#changing the value at index 3
+rasul[3] = 'musa'
+print(rasul[3])
 
 course = {'ctn': ['CS255', 'CS251'], 'it': ['CS240', 'CS259'], 'math': 'CS249'}
 print(course)
 #printing value at index 'it'
 print(course['it'])
+course['math'] = ['CS248', 'CS249']
+print(course['math'])
