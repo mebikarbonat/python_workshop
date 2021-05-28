786aa804d4b061b6d36a55529bd7326851e500f3
Muhammad Azizi Mohd Ariffin <azizi.mohdariffin@gmail.com>
2021-05-29 00:35:19 +0800

    2.3

---
+++ worksheet2.3.py
@@ -0,0 +1,16 @@
+#Muhammad Azizi Mohd Ariffin
+#mazizi@fskm.uitm.edu.my
+
+name = 'Ali '
+hobby = 'fishing'
+combining = name + hobby
+print(combining)
+
+if 'fishing' in combining :
+    print('The person hobby is fishing')
+else:
+    print('The person hobby is not fishing')
+
+##get the string length and multiply it with 2
+length = len(combining)
+print(length * 2)
