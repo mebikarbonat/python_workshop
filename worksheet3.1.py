7a71e656b5ad5fc99aaf4156053dcc1876508ebd
Muhammad Azizi Mohd Ariffin <azizi.mohdariffin@gmail.com>
2021-05-29 01:04:09 +0800

    3.1

---
+++ worksheet3.1.py
@@ -0,0 +1,12 @@
+#Muhammad Azizi Mohd Ariffin
+#mazizi@fskm.uitm.edu.my
+
+rasul = ['adam', 'idris', 'nuh', 'hud', 'salih']
+print(rasul)
+#Printing value at index 3
+print(rasul[3])
+
+course = {'ctn': ['CS255', 'CS251'], 'it': ['CS240', 'CS259'], 'math': 'CS249'}
+print(course)
+#printing value at index 'it'
+print(course['it'])
