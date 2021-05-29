eed6ddd00d54c13ff282d761a02583194792feb5
Muhammad Azizi Mohd Ariffin <azizi.mohdariffin@gmail.com>
2021-05-30 01:21:30 +0800

    5.3

---
+++ worksheet5.3.py
@@ -0,0 +1,23 @@
+#Muhammad Azizi Mohd Ariffin
+#mazizi@fskm.uitm.edu.my
+
+from netmiko import ConnectHandler
+linux = {
+        'device_type': 'linux',
+        'ip': '192.168.114.13',
+        'username': 'python',
+        'password': 'python123',
+        'secret': 'python123',
+        'port': 22,
+        'verbose': True
+        }
+connection = ConnectHandler(**linux)
+output = connection.send_command('uname -a')
+print('This is your kernel version: \n')
+print(output)
+
+#installing apache2 web server
+connection.enable()
+output = connection.send_command('apt install apache2')
+print(output)
+connection.exit_enable_mode()
