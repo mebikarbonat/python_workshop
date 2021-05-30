10d616e5a5b0e65468ef3ff066d62990dbdbbbe2
Muhammad Azizi Mohd Ariffin <azizi.mohdariffin@gmail.com>
2021-05-30 13:10:54 +0800

    7.1

--- worksheet7.1.py
+++ worksheet7.1.py
@@ -1,15 +1,22 @@
 #Muhammad Azizi Mohd Ariffin
 #mazizi@fskm.uitm.edu.my
 
-def calculateCircleArea(r) :
-    area = 3.14 * (r ** 2)
-    return area
-def goodbye() :
-    print ("Goodbye..end of program")
+import pandas as pd
 
-radiusList = [43,64,5,4,3,10]
-for r in radiusList:
-    temp = calculateCircleArea(r)
-    temp = round(temp, 2)
-    print('The Circle Area with radius ' + str(r) + ': ' + str(temp))
-goodbye()
+def convertToFeet(m) :
+    return( m * 3.281)
+
+df = pd.read_csv('nile.csv')
+print(df.head())
+
+runConvert = df.apply(lambda row: convertToFeet(row['Flood']), axis=1)
+df['feet'] = runConvert
+print(df.head())
+print('\n')
+print('The mean of flood is ' + str(df['Flood'].mean()))
+print('The median of flood is ' + str(df['Flood'].median()))
+print('The mean of flood in feet is ' + str(df['feet'].mean()))
+print('The median of flood in feet is ' + str(df['feet'].median()))
+
+#storing the df to csv file
+df.to_csv(r'nile_feet.csv', index=False)
